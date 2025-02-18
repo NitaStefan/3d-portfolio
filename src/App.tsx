1897659520325a524428a7f2940560b66569ed7f
sn <scarsan62@gmail.com>
2025-02-18 17:38:30 +0200

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero"
+import Projects from "./components/Projects"
+import Skills from "./components/Skills"
+
+function App() {
+  return (
+    <main className="max-w-5xl mx-auto">
+      <Hero />
+      <Skills />
+      <Projects />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return (
+    <section className="border-[5px] border-lotion/20 h-screen flex justify-center items-center">
+      <div>
+        <h1 className="text-xl">Hero Section</h1>
+        <p>This is the hero section of the portfolio.</p>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
