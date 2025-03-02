cfc0e20ec0cdb01b300cf77e7eb32f8d1e0d08de
sn <scarsan62@gmail.com>
2025-03-02 13:28:03 +0200

    finished projects section

--- src/components/ui/DesktopMobileViews.tsx
+++ src/components/ui/DesktopMobileViews.tsx
@@ -6,11 +6,22 @@ const DesktopMobileViews = ({
   toggle: () => void;
 }) => {
   return (
-    <div className="bg-medium-gray absolute top-0 right-0 flex flex-col items-center rounded-md p-[5px]">
-      <p className="text-sm">{showMobile ? "Mobile View" : "Desktop view"}</p>
-      <button onClick={toggle} className="cursor-pointer">
-        <span>Desktop|</span>
-        <span>|Mobile</span>
+    <div className="bg-medium-gray absolute top-[20px] right-[2%] flex w-[85px] flex-col items-center rounded-md p-[5px] max-md:px-0 md:top-[120px] md:right-[18%] md:w-[110px]">
+      <p className="text-xs font-light md:text-sm">
+        {showMobile ? "Mobile View" : "Desktop View"}
+      </p>
+      <button
+        onClick={toggle}
+        className="mt-[4px] flex cursor-pointer gap-x-[8px]"
+      >
+        <img
+          src="/icons/monitor.svg"
+          className={`rounded-md p-[5px] max-md:py-0 ${!showMobile && "bg-primary"}`}
+        />
+        <img
+          src="/icons/mobile.svg"
+          className={`rounded-md p-[5px] max-md:py-[1px] ${showMobile && "bg-primary"}`}
+        />
       </button>
     </div>
   );
